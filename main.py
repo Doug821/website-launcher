3b020b536a236556f18eac9024d8a784657e20bc
Herson Douglas <dougsilva821@gmail.com>
2024-04-16 19:23:52 -0300

    Refactor OpenWebsiteExtension class to OpenUrlExtension class in main.py Update manifest.json description in manifest.json Add versions.json file with required API version and commit details Update required_api_version in versions.json to ^2.0.0 Update required_api_version in manifest.json to ^2.0.0 Add OpenWebsiteExtension class and KeywordQueryEventListener class in main.py Add manifest.json file with extension details

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import webbrowser
 
 from ulauncher.api.client.EventListener import KeywordQueryEventListener
@@ -5,8 +6,17 @@ from ulauncher.api.client.Extension import Extension
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 from ulauncher.api.shared.action.RenderResultListAction import \
     RenderResultListAction
+from ulauncher.api.shared.action.ShowNotificationAction import \
+    ShowNotificationAction
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
+
+def is_valid_url(url):
+    regex = re.compile(
+        r'^(?:http|https)://',
+        re.IGNORECASE
+    )
+    return re.match(regex, url) is not None
 
 class OpenUrlExtension(Extension):
 
@@ -15,8 +25,15 @@ class OpenUrlExtension(Extension):
         self.subscribe(KeywordQueryEventListener(), self.on_keyword_query)
 
     def on_keyword_query(self, event):
-        # Default URL if none provided
-        query = event.get_argument() or 'https://github.com/Doug821/website-launcher'
+        query = event.get_argument()
+        if not query or not is_valid_url(query):
+            return RenderResultListAction([
+                ExtensionResultItem(icon='images/icon.png',
+                                    name='Invalid URL',
+                                    description='Please enter a valid URL starting with http:// or https://',
+                                    on_enter=ShowNotificationAction("Invalid URL entered"))
+            ])
+
         items = [
             ExtensionResultItem(icon='images/icon.png',
                                 name='Open URL',
