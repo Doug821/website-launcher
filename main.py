6db3c386dfee875c613c3374d0f12ee30239d809
Herson Douglas <dougsilva821@gmail.com>
2024-04-16 19:20:10 -0300

    Refactor OpenWebsiteExtension class to OpenUrlExtension class in main.py Update manifest.json description in manifest.json Add versions.json file with required API version and commit details

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 import webbrowser
 
-from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.client.EventListener import KeywordQueryEventListener
 from ulauncher.api.client.Extension import Extension
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 from ulauncher.api.shared.action.RenderResultListAction import \
     RenderResultListAction
-from ulauncher.api.shared.event import ItemEnterEvent, KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
 
-class OpenWebsiteExtension(Extension):
+class OpenUrlExtension(Extension):
 
     def __init__(self):
-        super().__init__()
-        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        super(OpenUrlExtension, self).__init__()
+        self.subscribe(KeywordQueryEventListener(), self.on_keyword_query)
 
-class KeywordQueryEventListener(EventListener):
-
-    def on_event(self, event, extension):
-        # Retrieve URL from extension preferences
-        url = extension.preferences['website_url']
+    def on_keyword_query(self, event):
+        # Default URL if none provided
+        query = event.get_argument() or 'https://github.com/Doug821/website-launcher'
         items = [
             ExtensionResultItem(icon='images/icon.png',
-                                name='Open Website',
-                                description='Click to open your configured website: {}'.format(url),
-                                on_enter=OpenUrlAction(url))
+                                name='Open URL',
+                                description='Press Enter to open: %s' % query,
+                                on_enter=OpenUrlAction(query))
         ]
-
         return RenderResultListAction(items)
 
+
 if __name__ == '__main__':
-    OpenWebsiteExtension().run()
+    OpenUrlExtension().run()
