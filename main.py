0bcdac52ccdf1a49e3052ff7a196cd11f8104a34
Herson Douglas <dougsilva821@gmail.com>
2024-04-16 20:30:11 -0300

    Refactor OpenWebsiteExtension class to OpenUrlExtension class and update event listeners in main.py

--- main.py
+++ main.py
@@ -10,13 +10,26 @@ from ulauncher.api.shared.action.RenderResultListAction import \
 from ulauncher.api.shared.event import ItemEnterEvent, KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
+ICON_PATH = 'images/icon.png'
+DEFAULT_URL_PROTOCOL = 'http://'
+
+
+def format_url(url):
+    if not url.startswith(('http://', 'https://')):
+        url = DEFAULT_URL_PROTOCOL + url
+    return url
+
+
+def open_url(url):
+    webbrowser.open_new_tab(format_url(url))
+
 
 class OpenUrlExtension(Extension):
-
     def __init__(self):
         super(OpenUrlExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
         self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
@@ -32,12 +45,14 @@ class KeywordQueryEventListener(EventListener):
 
         return RenderResultListAction(items)
 
+
 class ItemEnterEventListener(EventListener):
     def on_event(self, event, extension):
         query = event.get_data() or ''
         if not query.startswith(('http://', 'https://')):
             query = 'http://' + query
-        webbrowser.open_new_tab(query)
+        open_url(query)
+
 
 if __name__ == '__main__':
     OpenUrlExtension().run()
