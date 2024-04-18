c8a39a6fef1bc4fa0c3dd9264f31f1b9569ef421
Herson Douglas <dougsilva821@gmail.com>
2024-04-18 07:33:12 -0300

    Improve user feedback with better messages when no bookmarks are found or a new URL is bookmarked.

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ class KeywordQueryEventListener(EventListener):
                 for url in bookmarks.items():
                     items.append(ExtensionResultItem(
                         icon=ICON_PATH,
-                        name='Open Bookmark',
-                        description='URL: ' + url[1],
+                        name='url[1]',
+                        description='Open bookmark: {}'.format(url[0]),
                         on_enter=ExtensionCustomAction(url[1])
                     ))
             else:
